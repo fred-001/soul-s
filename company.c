a25b8bb699fe00828aaef3862abf7d3a50586bee
fred-001 <158086568+fred-001@users.noreply.github.com>
2024-02-20 15:21:54 +0300

    Add files via upload

---
+++ company.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main (){
+// varibles
+float salary, bonus = 0.0;
+int year of service;
+
+// prompt user
+printf("Enter your salary: ");
+scanf("%f", &salary);
+
+printf("Enter your years of services:");
+scanf("%d", &year of service);
+
+//calculate bonus
+if(years og services>5){
+    bonus =0.05 * salary;
+}
+//display
+printf("\nYour net bonus amount: %.2f\n ,bonus");
+
+
+
+
+return 0;
+
+}
