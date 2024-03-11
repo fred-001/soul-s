bd97eabdd500cae01f3c910e3155481d3074d360
fred-001 <158086568+fred-001@users.noreply.github.com>
2024-03-11 23:08:36 +0300

    Create Compare no.c

---
+++ Compare no.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){     
+printf("Enter an integer number:");
+scanf("%d",&number);
+if(number < 100)
+  }
+printf(" your number is smaller than a 100");
+}else
+{
+printf("your number contains more than two digits");
+}
+return 0;
+}
